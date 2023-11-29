dfd973022300a36fa1046d43b70b207081a1830a
Ing-gogo <gloriaingabire71@gmail.com>
2023-11-29 06:57:15 -0800

    task14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 b = 0
-for a in range(ord('z'), ord('a') -1, -1):
+for a in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(a - b)), end=""
-        b = 32 if a == 0 else 0
+        b=32 if a == 0 else 0
