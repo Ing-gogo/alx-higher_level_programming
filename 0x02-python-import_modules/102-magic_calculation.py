1a563de868c16f3658fa451c957e8e03d8b14a84
Ing-gogo <gloriaingabire71@gmail.com>
2023-11-30 07:12:42 -0800

    advanced

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!usr/bin/python
+from magic_calculation_102 import add, sub
+
+def magic_calculation(a,b):
+    if a < b:
+        c = add(a, b)
+        for x in range(4, 6):
+            c = add(c, x)
+            return (c)
+        else:
+            return sub(a, b)
