4305dd6fab31a4baa6d84fcd4a3b2ad7ec44c3bf
Ing-gogo <gloriaingabire71@gmail.com>
2023-12-18 08:20:10 -0800

    task3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    r = 0
+
+    try:
+        q = a / b
+    except (TypeError, ZeroDivisionError):
+        q = None
+    finally:
+        print("Inside result: {}".format(q))
+        return (q)
 
 
