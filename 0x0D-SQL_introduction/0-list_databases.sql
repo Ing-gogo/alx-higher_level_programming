0027c6b9064cdda41b0332725d7fd0ee037cd09b
Ing-gogo <gloriaingabire71@gmail.com>
2024-02-13 05:18:24 -0800

    mandatory task

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,4 @@
 --script showing list of all databases.
 SHOW DATABASES;
 
+
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 --script that deletes an existing database.
-DROP DATABASE IF EXISTS `hbtn_0n_0`;
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
 
